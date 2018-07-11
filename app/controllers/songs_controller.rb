d5dcab5c7971f7f3c360b044776c2aa41bd9ccfa
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-07-10 20:56:47 -0400

    Strong Params for song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,15 +4,21 @@ class SongsController < ApplicationController
     @songs = Song.all
   end
 
-  def create
-    @song = Song.new
-    @song.name = params[:song][:name]
-    @song.save
-
-    redirect_to song_path(@song)
-  end
-
   def show
     @song = Song.find(params[:id])
   end
+
+  def create
+    @song = Song.create(song_params)
+    # @song.name = params[:song][:name]
+    @song.save
+
+    redirect_to song_url(@song)
+  end
+
+  private
+
+     def song_params
+      params.require(:song).permit(:name)
+     end
 end
