4dfb654c66813db6f38075f142d909d4c05fe55e
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-07-10 20:43:44 -0400

    Show page for songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,10 +4,12 @@ class SongsController < ApplicationController
     @songs = Song.all
   end
 
-  def new
-  end
-
   def create
+    @song = Song.new
+    @song.name = params[:song][:name]
+    @song.save
+
+    redirect_to song_path(@song)
   end
 
   def show
